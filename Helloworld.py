e1cda4d89b82484e82efe23e1d5192453757fffa
Eugeniozhu <137764212+Eugeniozhu@users.noreply.github.com>
2024-11-27 22:26:27 +0500

    Add files via upload

---
+++ Helloworld.py
@@ -0,0 +1,20 @@
+print(12+81-1)
+
+
+#1st program
+print((9**0.5)*5)
+
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+one = 2*2+2
+two = 2*(2+2)
+print(one)
+print(two)
+print( one == two)
+
+#4th program
+number = "123.456"
+converted_number = int(float(number))
+print(converted_number-119)# я не понял для чего делить на 10, но 4 на экран вывел
